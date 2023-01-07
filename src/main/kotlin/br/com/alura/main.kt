2989940c7e43ca6d9ba393725a716833e3b16bc8
Kleiton Utida <kleiton.utida@live.com>
2023-01-07 01:49:26 -0300

    Knowing collections

---
+++ src/main/kotlin/br/com/alura/main.kt
@@ -0,0 +1,40 @@
+package br.com.alura
+
+fun main() {
+
+    val banco: BancoDeNomes = BancoDeNomes()
+
+    val nomesSalvos: Collection<String> = banco.nomes
+
+    banco.salva("Alex")
+    println(nomesSalvos)
+    println(BancoDeNomes().nomes)
+}
+
+class BancoDeNomes() {
+    val nomes: Collection<String> get() = dados.toList()
+
+    fun salva(nome: String) {
+        dados.add(nome)
+    }
+
+    companion object {
+        private val dados = mutableListOf<String>()
+    }
+}
+
+fun testaColecao() {
+    val nomes: Collection<String> = mutableListOf("Alex",
+            "Fran",
+            "Gui",
+            "Maria",
+            "Ana")
+    println(nomes)
+
+    for (nome in nomes.iterator()) {
+        println(nome)
+    }
+
+    println("Tem o nome Alex? ${nomes.contains("Alex")}")
+    println("Tamanho da coleção: ${nomes.size}")
+}
