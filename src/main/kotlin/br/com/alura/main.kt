821711ca301676b1807a1c523ab49e9d38fdbcf4
Kleiton Utida <klhutida@hotmail.com>
2023-01-09 19:42:53 -0300

    Read and filters on Map

--- src/main/kotlin/br/com/alura/main.kt
+++ src/main/kotlin/br/com/alura/main.kt
@@ -2,7 +2,71 @@ package br.com.alura
 
 fun main() {
 
-    val pedidos: MutableMap<Int, Double> = mutableMapOf<Int, Double>(Pair(1, 20.0), Pair(2, 34.0), 3 to 50.0)
+    val pedidos: MutableMap<Int, Double> = mutableMapOf(
+        Pair(1, 20.0),
+        Pair(2, 34.0),
+        3 to 50.0,
+        4 to 100.0,
+        5 to 150.0,
+        6 to 80.0,
+        7 to 60.0,
+        8 to 200.0
+    )
+
+    // Exibido valor nulo independentemente se ele existir ou não
+    println(pedidos.get(4))
+
+    // Exibido valor nulo que foi cadastrado para a chave 4
+    println(pedidos.getValue(4))
+
+    // Print valor nulo de uma chave inexistente
+    println(pedidos.get(5))
+
+    // Exceção, pois valor não existe
+    // println(pedidos.getValue(5))
+
+    // Retorna o valor da chave, caso contrário será retornado uma condição especificada
+    println(pedidos.getOrElse(1) { "Pedido inexistente" })
+
+    // Retorna o valor da chave, caso contrário será retornado uma condição especificada
+    println(pedidos.getOrElse(0) { "Pedido inexistente" })
+
+    // Retorna o valor da chave, caso contrário retorna um valor padrão
+    println(pedidos.getOrDefault(1, null))
+    println(pedidos.getOrDefault(1, -1.0))
+
+    // Retorna o valor da chave, caso contrário retorna um valor padrão
+    println(pedidos.getOrDefault(0, null))
+    println(pedidos.getOrDefault(0, -1.0))
+
+    // Exibe todas as chaves do Map
+    println(pedidos.keys)
+    for (numeroPedido in pedidos.keys) {
+        println("Pedido: $numeroPedido")
+    }
+
+    // Exibe todos os valores do Map
+    println(pedidos.values)
+    for (valorPedido in pedidos.values) {
+        println("Valor do Pedido: $valorPedido")
+    }
+
+    // Filtrar pela chave e pelo valor
+    val pedidosFiltrados = pedidos.filter { (numero, valor) ->
+        numero % 2 == 0 && valor > 50.0
+    }
+    println(pedidosFiltrados)
+
+    // Filtrar pelos valores
+    val pedidosPorValor = pedidos.filterValues { valor -> valor > 40.0 }
+    println(pedidosPorValor)
+
+    // Filtrar pelas chaves
+    val pedidosPorNumero = pedidos.filterKeys { numero -> numero % 2 == 1 }
+    println(pedidosPorNumero)
+}
+
+fun testaMap(pedidos: MutableMap<Int, Double>) {
     println(pedidos)
 
     val pedido: Double? = pedidos[3]
